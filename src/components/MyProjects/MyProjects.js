adfaec5d2cd5142c8e053e101567411079fbfe58
KalyanSreekar <kalyansreekarj@gmail.com>
2021-08-06 12:02:52 +0530

    responive issue in projects

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -4,6 +4,8 @@ import AnimatedBtn from "../AnimatedButton/AnimatedBtn";
 import { motion, useAnimation } from "framer-motion";
 import lr from "../../assets/loansride.jfif";
 import fx from "../../assets/fixation.webp";
+let box = document.getElementById("root");
+let width = box.offsetWidth;
 const MyProjects = (props) => {
   const firstBox = useAnimation();
   const firstboxtext = useAnimation();
@@ -12,116 +14,147 @@ const MyProjects = (props) => {
   const [anim, setanim] = useState([false, false]);
   const startbox = () => {
     setanim([true, anim[1]]);
+    if (width > 600) {
+      firstBox.start({
+        width: 600,
+        transition: { duration: 0.5 },
+      });
+      firstboxtext.start({
+        opacity: 1,
+        display: "flex",
+        transition: { delay: 0.5 },
+      });
+      return;
+    }
     firstBox.start({
-      width: 600,
+      scale: 0.8,
+      width: width,
+      margin: 0,
+      justifyContent: "flex-start",
+      height: 260,
       transition: { duration: 0.5 },
     });
     firstboxtext.start({
       opacity: 1,
+      display: "flex",
       transition: { delay: 0.5 },
     });
   };
   const startbox2 = () => {
     setanim([anim[0], true]);
+    if (width > 600) {
+      secondBox.start({
+        width: 600,
+        transition: { duration: 0.5 },
+      });
+      secondboxtext.start({
+        opacity: 1,
+        display: "flex",
+        transition: { delay: 0.5 },
+      });
+      return;
+    }
     secondBox.start({
-      width: 600,
+      scale: 0.8,
+      height: 260,
+      justifyContent: "flex-start",
+      margin: 0,
+      width: width,
       transition: { duration: 0.5 },
     });
     secondboxtext.start({
       opacity: 1,
+      display: "flex",
       transition: { delay: 0.5 },
     });
   };
   return (
     <React.Fragment>
       <motion.h1
-        style={{
-          opacity: 1,
-          marginBottom: 50,
-          padding: 0,
-          marginTop: -50,
-        }}
-        className={props.darkmode ? "themedarktext" : "themelighttext"}
+        className={
+          props.darkmode ? "heading themedarktext" : "heading themelighttext"
+        }
       >
         Major Projects
       </motion.h1>
-      <motion.div
-        className="projectdiv"
-        style={{
-          height: 150,
-          overflow: "hidden",
-          width: 150,
-          display: "flex",
-          alignItems: "center",
-          marginBottom: 30,
-          cursor: "pointer",
-        }}
-        animate={firstBox}
-        onClick={startbox}
-      >
-        {!anim[0] && (
-          <motion.img
-            style={{ width: 150, height: 150 }}
-            src={lr}
-            whileHover={{ scale: 1.2 }}
-            transition={{ duration: 0.4 }}
-            alt="Loansride logo"
-          />
-        )}
-        {anim[0] && (
-          <img
-            style={{ width: 150, height: 150 }}
-            src={lr}
-            alt="Loansride logo"
-          />
-        )}
+      <motion.div className="projectdiv" animate={firstBox} onClick={startbox}>
+        <motion.div
+          style={{ minWidth: 150, minHeight: 150, overflow: "hidden" }}
+        >
+          {!anim[0] && (
+            <motion.img
+              style={{ width: "100%", height: "100%", flex: 1 }}
+              src={lr}
+              whileHover={{ scale: 1.2 }}
+              transition={{ duration: 0.4 }}
+              alt="Loansride logo"
+            />
+          )}
+          {anim[0] && (
+            <img
+              style={{ width: 150, height: 150 }}
+              src={lr}
+              alt="Loansride logo"
+            />
+          )}
+        </motion.div>
         <motion.p
           className={props.darkmode ? "themedarktext" : "themelighttext"}
-          style={{ opacity: 0, marginLeft: 10 }}
+          style={{
+            opacity: 0,
+            display: "none",
+            padding: 10,
+          }}
           animate={firstboxtext}
         >
-          Loansride is online banking project that connects banks and people
+          {width >= 600 &&
+            `Loansride is online banking project that connects banks and people
           offering various types of loans and various offers and services for
-          banks and NBFCs.
+          banks and NBFCs.`}
+          {width < 600 &&
+            `Loansride is online banking project that connects banks and people
+          offering various types of loans and services`}
         </motion.p>
       </motion.div>
       <motion.div
         className="projectdiv"
-        style={{
-          height: 150,
-          overflow: "hidden",
-          width: 150,
-          display: "flex",
-          alignItems: "center",
-          cursor: "pointer",
-        }}
         animate={secondBox}
         onClick={startbox2}
       >
-        {!anim[1] && (
-          <motion.img
-            style={{ width: 150, height: 150 }}
-            whileHover={{ scale: 1.2 }}
-            transition={{ duration: 0.4 }}
-            src={fx}
-            alt="Fixation logo"
-          />
-        )}
-        {anim[1] && (
-          <motion.img
-            style={{ width: 150, height: 150 }}
-            src={fx}
-            alt="Fixation logo"
-          />
-        )}
+        <div style={{ minWidth: 150, minHeight: 150, overflow: "hidden" }}>
+          {!anim[1] && (
+            <motion.img
+              style={{ width: 150, height: 150 }}
+              whileHover={{ scale: 1.2 }}
+              transition={{ duration: 0.4 }}
+              src={fx}
+              alt="Fixation logo"
+            />
+          )}
+          {anim[1] && (
+            <motion.img
+              style={{ width: 150, height: 150 }}
+              src={fx}
+              alt="Fixation logo"
+            />
+          )}
+        </div>
         <motion.p
           className={props.darkmode ? "themedarktext" : "themelighttext"}
-          style={{ opacity: 0, marginLeft: 10 }}
+          style={{
+            opacity: 0,
+            padding: 10,
+            display: "none",
+          }}
           animate={secondboxtext}
         >
-          Fixation is a mobile application the one can use to avail services for
+          {width >= 600 &&
+            `Fixation is a mobile application the one can use to avail services for
           your motor vehicles. You can get your vehicle serviced or get a
-          mechanic to your doorstep. All solutions for your motor vehicle
+          mechanic to your doorstep. All solutions for your motor vehicle`}
+          {width < 600 &&
+            `Fixation is a mobile application the one can use to avail services for
+          your motor vehicles. `}
         </motion.p>
       </motion.div>
       <AnimatedBtn darkmode={props.darkmode} message="Contact Me" />
